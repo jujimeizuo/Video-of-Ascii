338eb70d7337ae9ffad442460236b2b5db0ed952
jujimeizuo <3058852685@qq.com>
2022-01-09 21:21:16 +0800

    first commit

---
+++ video_to_jpg.py
@@ -0,0 +1,23 @@
+import cv2
+videoFile = 'vedio.mp4'
+outputFile = 'url'
+
+vc = cv2.VideoCapture(videoFile)
+print(vc)
+c = 1
+if vc.isOpened():
+    rval, frame = vc.read()
+else:
+    print('open-Error!')
+    rval = False
+
+timeF = 100  #视频帧计数间隔次数
+while rval:
+    print("ok")
+    rval, frame = vc.read()
+    if c % timeF == 0:
+        print(2)
+        cv2.imwrite(outputFile + str(int(c / timeF)) + '.jpg', frame)
+    c += 1
+    cv2.waitKey(1)
+vc.release()
