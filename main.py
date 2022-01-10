ed68908b45958ee55e38dbae2a1c8cfe8e5c675f
狙击美佐 <fengzetao.zed@foxmail.com>
2022-01-10 18:34:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -144,6 +144,6 @@ class V2Char(CharFrame):
 
 if __name__ == "__main__":
     # sys.path.append(os.path.abspath(os.path.join(os.getcwd(), "..")))  # 当前项目路径加入
-    v2char = V2Char('./vedio.mp4')
+    v2char = V2Char('./video.mp4')
     v2char.play()
 
